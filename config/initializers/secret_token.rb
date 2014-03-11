4bee869179b04be8d6a519c9a1c924ffeecf6d5a
Jethro CM Kim <jethro.cm.kim@gmail.com>
2014-03-11 10:50:45 -0700

    first commit. core functionalities functional

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeDisplay::Application.config.secret_key_base = '3ee4304aa66c5178dd5820c56c7e7e62a6c4582caf9e2db3efbe6b4895c34030f7261964527c0f4a1793b4f60b2a3b1bd0aea4fbca10e1fbacde3dd1a707ca5e'
